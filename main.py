2101bd1f7fa42592ba2daae884104ca821387ab5
samadgod569 <craftersmcnavigators@gmail.com>
2026-04-12 13:37:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "<h1>My VPS site is live 🚀</h1>"
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=4003)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4003, debug=True)
