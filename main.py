eb199d9452f0a9f6e0983f2cf192008f2049e2d1
samadgod569 <craftersmcnavigators@gmail.com>
2026-04-14 07:33:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=4003, debug=True)
+    app.run(host="0.0.0.0", port=4002, debug=True)
