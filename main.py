b812129504faec877429805e158fc615bd338af2
samadgod569 <craftersmcnavigators@gmail.com>
2026-05-23 19:12:00 +0530

    Remove Flask app and add print statement

--- main.py
+++ main.py
@@ -1,10 +1,1 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=40000, debug=True)
+print(2+3)
